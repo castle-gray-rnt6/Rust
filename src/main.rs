f32837a1ebfe134e223029867ef266a8df4d7bd7
Kryzar <clapped.hesitancy332@anonaddy.me>
2025-09-01 14:57:47 -0600

    This doesn't work

--- src/main.rs
+++ src/main.rs
@@ -4,32 +4,19 @@ mod cassels;
 use cassels::loop_over_roots;
 
 use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 fn main() -> std::io::Result<()> {
     let file_tables = File::create("tables.txt")?;
     let file_output = File::create("output.txt")?;
-    let inputs = [(19,              9),
-                  (2*2*3*7,         8),
-                  (2*2*3*5,         8),
-                  (7*13,            7),
-                  (2*2*3*5*7,       7),
-                  (31,              6),
-                  (29,              6),
-                  (23,              6),
-                  (2*2*13,          6),
-                  (3*5*11,          6),
-                  (3*3*7,           6),
-                  (3*3*5,           6),
-                  (3*7*11*13,       5),
-                  (5*13,            5),
-                  (2*2*3*5*7*11,    5),
-                  (5*19,            4),
-                  (5*17,            4),
-                  (2*2*3*5*7*11*13, 4),
-                  (2*2*2*3*3*5*7,   4),
-                  (2*2*2*2*3*5,     4)];
+    let inputs = [(1, 1)];
     for (n0, len) in inputs {
         loop_over_roots(n0, len, &file_tables, &file_output);
+    }
+
+    let lines = BufReader::new(&file_output).lines();
+    for line in lines {
+        println!("{}", "This is a line");
     }
 
     println!("All cases checked!");
